d1f90fc4470b98bc5307e51219d03ffde346c048
Akanshu soni <akanshusoni.ait@gmail.com>
2024-07-17 13:27:08 +0530

    doc update
    doc

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-YGPTNWAL.mjs";import"./chunk-PBR2VALS.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-FVXAOKGH.mjs";import"./chunk-PBR2VALS.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
